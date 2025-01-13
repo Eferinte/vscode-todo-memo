377d86043e7775dd1e0e84bdaa110b8d394664de
linxiaopeng <linxiaopeng@metacarbon.vip>
2025-01-13 19:50:16 +0800

    clean

--- src/extension.ts
+++ src/extension.ts
@@ -1,88 +1,31 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode'
-import { translator } from './translators'
-import { Divider } from './markdown'
-import { record, translateAndRecord } from './record'
 
-const testApiServiceAvailable = async () => {
-  // test api service
-  if (await translator.test()) {
-    vscode.window.showInformationMessage(
-      `connect to ${translator.getServiceName()} successfully !`,
-    )
-  } else {
-    vscode.window.showErrorMessage(
-      `connect to ${translator.getServiceName()} failed !`,
-    )
-  }
-}
+import { Divider } from './markdown'
 
 const init = async () => {
-  vscode.languages.registerHoverProvider('*', {
-    async provideHover(document, position) {
-      if (
-        !document.getWordRangeAtPosition(position) ||
-        !vscode.window.activeTextEditor
-      ) {
-        return
-      }
+  console.log('do init')
+}
 
-      // 取hover word ，有hover word内的部分选取时优先使用选取值
-      let word = document.getText(document.getWordRangeAtPosition(position))
-      let selectText = vscode.window.activeTextEditor.document.getText(
-        vscode.window.activeTextEditor.selection,
-      )
-      if (selectText && word.indexOf(selectText) > -1) {
-        word = selectText
-      }
-
-      const translatedWord = await translator.translate(word)
-      const serviceInfo = `Translated by : ${translator.getServiceName()}`
-      const apply = `[写入](command:translator.record)`
-
-      let hoverText = new vscode.MarkdownString(
-        `${word} => ${translatedWord}${Divider}${serviceInfo}${Divider}${apply}`,
-      )
-
-      hoverText.isTrusted = true
-
-      return new vscode.Hover(hoverText)
-    },
-  })
+const addMemo = () => {
+  vscode.window.showInformationMessage(`hello world`)
 }
 
 const applyUserConfig = () => {
   const targetLanguage = vscode.workspace
     .getConfiguration('translator')
     .get<string>('targetLanguage')
-  targetLanguage && translator.setTarget(targetLanguage)
 }
 
 export async function activate(context: vscode.ExtensionContext) {
   context.subscriptions.push(
-    vscode.commands.registerCommand('translator.record', record),
-  )
-
-  context.subscriptions.push(
-    vscode.commands.registerCommand(
-      'translator.translateAndRecord',
-      translateAndRecord,
-    ),
-  )
-
-  context.subscriptions.push(
-    vscode.commands.registerCommand(
-      'translator.testApiServiceAvailable',
-      testApiServiceAvailable,
-    ),
+    vscode.commands.registerCommand('translator.add-memo', addMemo),
   )
 
   init()
 
   applyUserConfig()
-
-  testApiServiceAvailable()
 }
 
 vscode.workspace.onDidChangeConfiguration((event) => {
